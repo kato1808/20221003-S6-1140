2a32e5d3299476c5894a4df7f89b44b3ce56c465
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-10-03 12:10:07 +0900

    202210031210

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowflag, setFaceShowflag] = useState(true);
+
+  const onClickCountup = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowflag(!faceShowflag);
+  };
+
+  if (num % 3 === 0) {
+    faceShowflag || setFaceShowflag(true);
+  } else {
+    faceShowflag && setFaceShowflag(false);
+  }
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowflag && <p>lallrkgk</p>}
     </>
   );
 };
